a25d6543cedb7cbf86526d56f8ad5d2786f5494e
Rali Dimitrov <ralidimitrovdimitrov@gmail.com>
2018-06-06 08:32:01 +0300

    Typo 3 ext

---
+++ typo3conf/ext/store_inventory/Classess/Domain/Repository/ProductRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MyVendor\StoreInventory\Domain\Repository;
+
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+/**
+ * Class ProductRepository
+ *
+ * @package MyVendor\StoreInventory\Domain\Repository
+ */
+class ProductRepository extends Repository
+{
+
+}
